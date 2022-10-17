faaaf7bb835414325e478fa9f21b88180cf8372c
AlexparraC <sworks2021rr@gmail.com>
2022-10-17 12:56:31 -0500

    primer commit arrays1

---
+++ server/01-for-each.js
@@ -0,0 +1,11 @@
+const letters = [ 'a', 'b', 'c'];
+
+for (let index = 0; index < letters.length; index++) {
+    let element = letters[index];
+    console.log(element);
+    }
+    const letters1 = [ 'a', 'b', 'c'];
+
+    letters1.forEach(item => console.log(item));
+    
+console.log("hello world"); 
